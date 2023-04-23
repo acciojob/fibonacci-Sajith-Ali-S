ac007f9d4500898bdfcfe54f4c86c484128033e4
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-04-23 09:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-
-function fibonacci(num) {
-    if(num==1)return 0
-    if(num==2)return 1
-    return fibonacci(num-1)+fibonacci(num-2)
+let fib = [0,1]
+function fibonacci(num){
+ 
+    for(let i = 2; i < num ; i++){
+        fib[i] = fib[i-1]+fib[i-2];
     }
+    return fib[num-1];
+  
+}
 module.exports = fibonacci;
